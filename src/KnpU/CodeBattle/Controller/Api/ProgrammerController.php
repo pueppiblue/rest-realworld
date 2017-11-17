ce31300b3bf209ab0b770297ea25d44322729cc9
pueppiblue <Nils.Freigang@gmail.com>
2017-11-17 21:26:11 +0100

    create programmers post endpoint
      - create programmer from json data in request
      - save programmmer
      - return string as response
      - mimic a client request with guzzle in testint.php

--- src/KnpU/CodeBattle/Controller/Api/ProgrammerController.php
+++ src/KnpU/CodeBattle/Controller/Api/ProgrammerController.php
@@ -3,17 +3,32 @@
 namespace KnpU\CodeBattle\Controller\Api;
 
 use KnpU\CodeBattle\Controller\BaseController;
-use Silex\Application;
+use KnpU\CodeBattle\Model\Programmer;
 use Silex\ControllerCollection;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\JsonResponse;
 
 class ProgrammerController extends BaseController
 {
     protected function addRoutes(ControllerCollection $controllers)
     {
-        // $controllers->post('/api/programmers', array($this, 'newAction'));
+        $controllers->post('/api/programmers', array($this, 'newAction'));
     }
 
+    public function newAction(Request $request)
+    {
+        $data = json_decode($request->getContent(), true);
+
+        $programmer = new Programmer($data['nickname'], $data['avatarNumber']);
+        $programmer->tagLine = $data['tagLine'];
+        $programmer->userId = $this->findUserByUsername('weaverryan')->id;
+
+        try {
+            $this->save($programmer);
+        } catch (\Exception $e) {
+            return 'Error when saving programmer resource: '.$e->getMessage();
+        }
+
+        return 'Programmer created and saved by me: The ALMIGHTY API';
+
+    }
 }
--- testing.php
+++ testing.php
@@ -5,9 +5,22 @@ require __DIR__.'/vendor/autoload.php';
 use Guzzle\Http\Client;
 
 // create our http client (Guzzle)
-$client = new Client('http://localhost:8000', array(
+$client = new Client('http://rest-realworld.local', array(
     'request.options' => array(
         'exceptions' => false,
     )
 ));
+
+$nickname = 'GeekDev'.random_int(1,199);
+$data = [
+    'nickname' => $nickname,
+    'tagLine' => 'A test programmer!',
+    'avatarNumber' => random_int(1,5)
+];
+
+$request = $client->post('/api/programmers', null, json_encode($data));
+$response = $request->send();
+
+echo $response;
+echo "\n\n";
 
