fde5bedf61bfed6cdb783e4e493b32b1964b4afd
pueppiblue <Nils.Freigang@gmail.com>
2017-11-20 18:28:35 +0100

    add JMS\Serializer and configure it as a public service

--- src/KnpU/CodeBattle/Application.php
+++ src/KnpU/CodeBattle/Application.php
@@ -3,6 +3,8 @@
 namespace KnpU\CodeBattle;
 
 use Doctrine\Common\Annotations\AnnotationReader;
+use JMS\Serializer\Naming\IdenticalPropertyNamingStrategy;
+use JMS\Serializer\SerializerBuilder;
 use KnpU\CodeBattle\Api\ApiProblem;
 use KnpU\CodeBattle\Api\ApiProblemException;
 use KnpU\CodeBattle\Battle\PowerManager;
@@ -207,6 +209,14 @@ class Application extends SilexApplication
 
         $this['api.validator'] = self::share(function () use ($app) {
             return new ApiValidator($app['validator']);
+        });
+
+        $this['serializer'] = self::share(function () use ($app) {
+            return SerializerBuilder::create()
+                ->setCacheDir($app['root_dir'] . '/cache/serializer')
+                ->setDebug($app['debug'])
+                ->setPropertyNamingStrategy(new IdenticalPropertyNamingStrategy())
+                ->build();
         });
     }
 
