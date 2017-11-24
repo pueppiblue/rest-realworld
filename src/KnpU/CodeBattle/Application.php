296b6143df5d5804c9e8b0f01c502191e84636d5
pueppiblue <Nils.Freigang@gmail.com>
2017-11-24 15:31:46 +0100

    install willdurand/Hateoas and register it as serializer service
      - pass a JMS Serializer instance to hateoas on instantiation

--- src/KnpU/CodeBattle/Application.php
+++ src/KnpU/CodeBattle/Application.php
@@ -3,6 +3,7 @@
 namespace KnpU\CodeBattle;
 
 use Doctrine\Common\Annotations\AnnotationReader;
+use Hateoas\HateoasBuilder;
 use JMS\Serializer\Naming\IdenticalPropertyNamingStrategy;
 use JMS\Serializer\SerializerBuilder;
 use KnpU\CodeBattle\Api\ApiProblem;
@@ -212,11 +213,11 @@ class Application extends SilexApplication
         });
 
         $this['serializer'] = self::share(function () use ($app) {
-            return SerializerBuilder::create()
+            $serializerBuilder = SerializerBuilder::create()
                 ->setCacheDir($app['root_dir'] . '/cache/serializer')
                 ->setDebug($app['debug'])
-                ->setPropertyNamingStrategy(new IdenticalPropertyNamingStrategy())
-                ->build();
+                ->setPropertyNamingStrategy(new IdenticalPropertyNamingStrategy());
+            return HateoasBuilder::create($serializerBuilder)->build();
         });
     }
 
